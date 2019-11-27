dcf454d66704b243a2a1df005b7c025ef906f9d2
Adam <adam.fraczkowski@gmail.com>
2019-11-27 19:53:31 +0100

    fixing project structure

---
+++ ColorTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace colors_recognition
+{
+    class ColorTest
+    {
+        // zmienne prywatne mogą być modyfikowane tylko w klasie
+
+        // ilość pytań w teście
+        public int testCases = 5;
+
+        // słownik w którym przechowujemy sobie nazwy kolorów i odnośniki do obrazków, postaci klucz -> wartość
+        public Dictionary<string, System.Drawing.Bitmap> colorsDict = new Dictionary<string, System.Drawing.Bitmap>();
+        private int actualProgress;
+        private List<string> actualTestQueue;
+        // tutaj będę przechowywał wyniki aktualnego testu
+        private int goodAnswersCnt;
+        private int badAnswersCnt;
+
+        // konstruktor. W uproszczeniu ten kod wykona się za każdym razem kiedy stworzę obiekt ColorTest. Inicjalizuję sobie tutaj zmienne. 
+        public ColorTest()
+        {
+            // Aby odwołać się do mojego słownika colorsDict, który jest w mojej klasie korzystam ze wskazania że zmienna jest elementem mojej klasy czyli this właśnie.
+            // dodaję kolory do mojego słownika
+            this.colorsDict.Add("niebieski", Properties.Resources.blue);
+            this.colorsDict.Add("czerwony", Properties.Resources.red);
+            this.colorsDict.Add("zielony", Properties.Resources.green);
+            this.colorsDict.Add("biały", Properties.Resources.white);
+            this.colorsDict.Add("żółty", Properties.Resources.yellow1);
+            this.colorsDict.Add("czarny", Properties.Resources.black);
+            this.colorsDict.Add("brązowy", Properties.Resources.brown);
+            this.colorsDict.Add("pomarańczowy", Properties.Resources.orange);
+            // ustawiam zmienną która będzie mi mówić o aktualnym progresie testu
+            this.actualProgress = 0;
+            // W tej liście będę przechowywał sobie kolejość w jakiej mają się pojawiać obrazki.
+            this.actualTestQueue = new List<string>();
+            this.goodAnswersCnt = 0;
+            this.badAnswersCnt = 0;
+
+        }
+        // przygotowanie kolejki
+        private void setActualTestQueue()
+        {
+            // opróżniam aktualną kolejkę
+            this.actualTestQueue.Clear();
+            // pętla w której losuję kolory ze słownika
+            // dopóki moja kolejka nie będzie miała odpowiedniej ilości testów to powtarzaj
+            while(this.actualTestQueue.Count<this.testCases)
+            {
+                // losuję liczbę od 0 do ilości kolorów w słowniku
+                // sprawdzam czy kolor nie został wcześniej wylosowany
+                Random randNum = new Random();
+                string actualColor = this.colorsDict.ElementAt(randNum.Next(0,this.colorsDict.Count)).Key;
+
+                if (!this.actualTestQueue.Contains(actualColor))
+                {
+                    // nie znaleziono w kolejce wylosowanego koloru. Dodaję go do tablicy
+                    this.actualTestQueue.Add(actualColor);
+                } else
+                {
+                    // wykonuj nadal pętlę
+                    continue;
+                }
+
+                
+            }
+
+        }
+        // Wywołanie kolejnego przykładu
+        public string getNextCase()
+        {
+            // Aktualizuję progres testu
+            if(this.actualProgress<this.testCases)
+            {
+                this.actualProgress = this.actualProgress + 1;
+                return actualTestQueue[this.actualProgress-1];
+            } else
+            {
+                return actualTestQueue[this.actualProgress];
+            }
+            
+        }
+        // przygotowanie nowego testu czyli wypełnienie kolejki ustawienie progresu, wyzerowanie odpowiedzi
+        public void startTest()
+        {
+
+            this.actualProgress = 0;
+            this.setActualTestQueue();
+            this.goodAnswersCnt = 0;
+            this.badAnswersCnt = 0;
+        }
+
+        // sprawdzenie odpowiedzi
+        // true w przypadku poprawnej odpowiedzi, false w przypadku złej
+        public bool checkAnswer(string answer)
+        {
+            if(answer.ToLower() == actualTestQueue[this.actualProgress-1])
+            {
+                this.goodAnswersCnt = this.goodAnswersCnt + 1;
+                return true;
+            } else
+            {
+                this.badAnswersCnt = this.badAnswersCnt + 1;
+                return false;
+            }
+        }
+
+        public int getActualProgress()
+        {
+            return this.actualProgress;
+        }
+
+        public string getGoodAnswers()
+        {
+            return this.goodAnswersCnt.ToString();
+        }
+
+        public string getBadAnswers()
+        {
+            return this.badAnswersCnt.ToString();
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace colors_recognition
+{
+    public partial class Form1 : Form
+    {
+
+        private ColorTest colorTest;
+        public bool restartFlag = false;
+        public int timeForAnswer = 30;
+        public int actualAnswerTime = 0;
+
+        public Form1()
+        {
+            this.colorTest = new ColorTest();
+            
+            InitializeComponent();
+            // konfiguruję progress bar
+            this.progressBar1.Minimum = 0;
+            this.progressBar1.Maximum = colorTest.testCases;
+            label3.Text = this.timeForAnswer.ToString();
+            // zmieniam labela
+            label1.Text = "0/"+ colorTest.testCases.ToString();
+            // tworzę nowy test
+            colorTest.startTest();
+        }
+        // ładowanie zdjęć do boxa
+        private void loadResourceToImageBox(string resourceColorName)
+        {
+            // load image from resources
+            pictureBox1.Image = colorTest.colorsDict[resourceColorName];
+            //pictureBox1.Load();
+        }
+
+        private void resetTimer()
+        {
+            label3.Text = this.timeForAnswer.ToString();
+            actualAnswerTime = 0;
+        }
+
+        private void switchState()
+        {
+
+
+            // w zależności od aktualnego progresu testu robię różne czynności
+            //resetuję timer
+            this.resetTimer();
+
+            string temporaryResourceName = "";
+            if (this.colorTest.getActualProgress() == 0)
+            {
+               
+                
+                // ustawiam flagę restartu na fałsz
+                this.restartFlag = false;
+                // rozpoczynam test nie sprawdzam żadnych odpowiedzi i idę dalej
+                temporaryResourceName = this.colorTest.getNextCase();
+                button1.Text = "Następny";
+                // wyświetlam fotografię
+                this.loadResourceToImageBox(temporaryResourceName);
+
+                // aktualizuję progres
+                this.progressBar1.Value = colorTest.getActualProgress();
+                // zmieniam labela
+                label1.Text = colorTest.getActualProgress().ToString() + "/" + colorTest.testCases.ToString();
+                // uruchamiam timer
+                timer1.Enabled = true;
+
+                // kończę wywołanie funkcji
+                return;
+            }
+
+            if (this.colorTest.getActualProgress() == this.colorTest.testCases)
+            {
+                // jestem przy ostatnim pytaniu. Czyszczę ImageBoxa, i wyświetlam rezultaty
+                if(!this.restartFlag)
+                {
+                    //zatrzymuję timer
+                    timer1.Enabled = false;
+                    this.colorTest.checkAnswer(textBox1.Text);
+                    MessageBox.Show("Wyniki \n Dobrze:"+colorTest.getGoodAnswers()+" \n Źle: "+colorTest.getBadAnswers());
+                    this.restartFlag = true;
+                    button1.Text = "Start !";
+                    pictureBox1.Image = null;
+
+                }
+                else
+                {
+                    colorTest.startTest();
+                    this.switchState();
+                }
+                return;
+            }
+
+            // w przypadku zwykłego kliknięcia sprawdzam wpisaną odpowiedź i idę dalej
+            this.colorTest.checkAnswer(textBox1.Text);
+            temporaryResourceName = this.colorTest.getNextCase();
+            this.loadResourceToImageBox(temporaryResourceName);
+            // aktualizuję progres
+            this.progressBar1.Value = colorTest.getActualProgress();
+            // zmieniam labela
+            label1.Text = colorTest.getActualProgress().ToString() + "/" + colorTest.testCases.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            this.switchState();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //aktualizuję timer
+            this.actualAnswerTime = this.actualAnswerTime + 1;
+            int timeToShow = this.timeForAnswer - this.actualAnswerTime;
+            label3.Text = timeToShow.ToString();
+            // gdy czas się skończy dla danego pytania
+            if (this.actualAnswerTime == this.timeForAnswer)
+            {
+                //resetuj timer (a w zasadzie zmienne timera)
+                this.resetTimer();
+                // przejdź do następnego
+                this.switchState();
+            }
+        }
+    }
+}
